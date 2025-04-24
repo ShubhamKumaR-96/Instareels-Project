ba1d05465df61a3d150f67cec6668a256e9f9d04
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-04-24 08:41:28 +0530

    Added query-hook for downloading videos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { getAllVideos } from './api/videos'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/hooks/apis/useFetchAllVideos.js
@@ -0,0 +1,16 @@
+import { getAllVideos } from "@/api/videos";
+import { useQuery } from "@tanstack/react-query";
+
+
+export default function useFetchAllvideos(){
+    const {isFetching:isVideoFetching,isError:isVideoError,isFetched:isVideoFetched,data:videos}=useQuery({
+        queryKey:"videos",
+        queryFn:getAllVideos(),
+        cacheTime:0
+    })
+
+    return{
+        isVideoFetched,isVideoError,isVideoFetching,videos
+    }
+
+}
